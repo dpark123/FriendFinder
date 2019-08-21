3194fdddc20c20262fb920702c3412b445f6c7c5
Donald Park <donaldjpark@gmail.com>
2019-08-20 18:35:15 -0700

    made the api path to properly list all user data

--- app/data/friends.js
+++ app/data/friends.js
@@ -14,9 +14,34 @@ var friendsArray = [
             4,
             1
           ]
+      },
+      {
+        "name":"Joe",
+        "photo":"https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+        "scores":[
+            1,
+            3,
+            4,
+            5,
+            3,
+            2,
+            2,
+            4,
+            1,
+            4
+          ]
       }
       
   ];
   
   // Note how we export the array. This makes it accessible to other files using require.
   module.exports = friendsArray;
+
+  var compareArray = [];
+  for (var i = 0; i<friendsArray.length; i++){
+    compareArray.push(friendsArray[i].scores);
+  }
+
+  console.log(compareArray);
+
+ var differenceArray = [];
--- server.js
+++ server.js
@@ -12,8 +12,8 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// require("./routing/apiRoutes")(app);
-// require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./app/public/home.html"));
